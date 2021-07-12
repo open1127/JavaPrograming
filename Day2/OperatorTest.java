02f72bb9f08c1f7996f72d21029ca6e5f29ad22d
open1127 <78148044+open1127@users.noreply.github.com>
2021-07-12 21:49:32 +0900

    Update OperatorTest.java

--- Day2/OperatorTest.java
+++ Day2/OperatorTest.java
@@ -5,31 +5,13 @@ class OperatorTest
 		int num1 = 10, num2 = 10, num3 =10;
 		int result1 = 0, result2 = 0;
 		
-		result1 = ++num1;		
-		result2 = num2++;		
+		result1 = ++num1;		//선치: 증감연산자가 앞에 있을때 증감후값이 대입됨
+		result2 = num2++;		//후치: 증감연산자가 뒤에 있을때 증감전값이 대입됨
 
-		System.out.println("num1 = " + num1 + " num2 = " + num2);
-		System.out.println("result1 = " + result1 + " result2 = " + result2);
-		System.out.println("num3++ " + num3++);
-		System.out.println("num3 " + num3);
+		System.out.println("num1 = " + num1 + " num2 = " + num2); 		//11 11
+		System.out.println("result1 = " + result1 + " result2 = " + result2);	//11 10
+		System.out.println("num3++ " + num3++);					//10
+		System.out.println("num3 " + num3);					//11
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
